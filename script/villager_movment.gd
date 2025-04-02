c215c559811cb3f3d8edf6421b0c3fdf6afbc12a
dovrv <123097352+dovrv@users.noreply.github.com>
2025-04-02 23:22:55 +0300

    Update villager_movment.gd

--- script/villager_movment.gd
+++ script/villager_movment.gd
@@ -1,6 +1,19 @@
 extends PathFollow2D
 
+var moving = true
 var speed = 0.1
+@onready var timer: Timer = $Villager/Timer
 
 func _process(delta):
-	progress_ratio += delta * speed
+	if moving == true:
+		progress_ratio += delta * speed
+
+func _ready():
+	timer.start(randf_range(10, 15))
+
+func _on_timer_timeout() -> void:
+	if moving == false:
+		moving = true
+	else: moving = false
+	
+	timer.start(randf_range(10, 15))
